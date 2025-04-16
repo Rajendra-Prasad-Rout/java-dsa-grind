5b9f46f8cd75b87016cf121ea5178d94296bc0fe
Rajendra Prasad Rout <rajendrarout8018@gmail.com>
2025-04-16 16:37:21 +0530

    Include .gitignore file

--- java/src/arrays/ArrayBasics.java
+++ java/src/arrays/ArrayBasics.java
@@ -1,4 +1,15 @@
 package arrays;
 
+/**
+ * A basic class to demonstrate array operations.
+ */
 public class ArrayBasics {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
 }
